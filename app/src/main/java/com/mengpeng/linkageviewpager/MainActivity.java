bca8da27bffc250694a6b3f8f806725eecf64782
rizkytegaryjs0x <wjx1234567@yeah.net>
2021-06-19 15:28:22 +0800

    自定义底部RadioGroupTabLayout

--- app/src/main/java/com/mengpeng/linkageviewpager/MainActivity.java
+++ app/src/main/java/com/mengpeng/linkageviewpager/MainActivity.java
@@ -2,12 +2,46 @@ package com.mengpeng.linkageviewpager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mengpeng.linkageviewpager.widget.LinkTab;
+import com.mengpeng.linkageviewpager.widget.RadioGroupTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroupTabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+
+        initTab();
+    }
+
+    private void initTab() {
+
+        List<LinkTab> list = new ArrayList<>();
+        list.add(new LinkTab("测试1", R.drawable.maintab_1_selector));
+        list.add(new LinkTab("测试2", R.drawable.maintab_2_selector));
+        list.add(new LinkTab("测试3", R.drawable.maintab_3_selector));
+        list.add(new LinkTab("测试4", R.drawable.maintab_4_selector));
+
+        tabLayout.addTab(list);
+        tabLayout.setRadioGroupTabLayoutBackground(R.color.colorBg);
+
+        tabLayout.setOnRadioGroupTabLayoutClickListener(
+                new RadioGroupTabLayout.setOnRadioGroupTabLayoutClickListener() {
+                    @Override
+                    public void onTabLayoutClickListener(int position) {
+                        Log.d("MainActovity", "下标：" + position);
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/mengpeng/linkageviewpager/widget/LinkTab.java
+++ app/src/main/java/com/mengpeng/linkageviewpager/widget/LinkTab.java
@@ -6,7 +6,44 @@ package com.mengpeng.linkageviewpager.widget;
  * 说明：
  */
 public class LinkTab {
+
     private String tabName;
     private int tabIcon;
     private int tabNameID;
+
+    public String getTabName() {
+        return tabName;
+    }
+
+    public void setTabName(String tabName) {
+        this.tabName = tabName;
+    }
+
+    public int getTabIcon() {
+        return tabIcon;
+    }
+
+    public void setTabIcon(int tabIcon) {
+        this.tabIcon = tabIcon;
+    }
+
+    public int getTabNameID() {
+        return tabNameID;
+    }
+
+    public void setTabNameID(int tabNameID) {
+        this.tabNameID = tabNameID;
+    }
+
+    public LinkTab(String tabName, int tabIcon) {
+        this.tabName = tabName;
+        this.tabIcon = tabIcon;
+    }
+
+    public LinkTab(int tabIcon, int tabNameID) {
+        this.tabIcon = tabIcon;
+        this.tabNameID = tabNameID;
+    }
+
+
 }
